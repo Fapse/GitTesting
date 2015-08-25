09c2935a72481cc57b8690617590aa3395e2391e
Fapse <fb-online@posteo.de>
2015-08-25 22:20:59 +0200

    Added second console message
    First commit to branch "SecondConsoleMessage"
    
    Signed-off-by: Fapse <fb-online@posteo.de>

--- GitTesting/src/com/fapse/gitTesting/GitTesting.java
+++ GitTesting/src/com/fapse/gitTesting/GitTesting.java
@@ -5,6 +5,7 @@ public class GitTesting {
 	public static void main(String[] args) {
 		System.out.println("Hello Git, my darling!");
 		//Hey it's obviously working!
+		System.out.println("This is my first branch!");
 	}
 
 }
